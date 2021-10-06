35ff57e69d549d53e6bacc79bd9b4655389d620c
mizraimmtz <mmartinez@qualitycode.com.mx>
2021-10-05 23:11:33 -0500

    create(se creo el proyecto de biblioteca): se creo el proyecto del control de la biblioteca en springboot y se bajaron las librerias necesarias para el proyecto. Ademas de que se hicieron los paquetes para la separacion de capas

---
+++ src/main/java/mx/com/qc/ControlBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package mx.com.qc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlBibliotecaApplication.class, args);
+	}
+
+}
